5a401acbcfdc8e20ddae2139909b664ff8c00245
LebowskiSama <anushvenkat101@gmail.com>
2020-10-10 15:06:04 +0530

    initial boilerplate

---
+++ src/components/materialTextField.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+
+export default function textField() {
+    return (
+        <TextField id="standard-basic materialField" label="Search..."></TextField>
+    )
+}
