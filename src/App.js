8c803dcff148592882e3f1ba9b32931fb8a79470
LebowskiSama <anushvenkat101@gmail.com>
2020-10-12 00:44:06 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -13,30 +13,50 @@ class App extends React.Component {
     super(props)
     this.state = {
       searchQuery: "",
-      omdbResponse: ""
+      omdbResponse: [],
+      tt: "",
+      title: ""
     }
     
     this.handleChange = this.handleChange.bind(this)
+    this.handleChoice = this.handleChoice.bind(this)
   }
 
   handleChange = e => {
     this.setState({searchQuery: e.target.value}, () => {
       axios.get("http://www.omdbapi.com/?apikey=2a5e533b&s="+this.state.searchQuery)
-      .then(response => this.setState({omdbResponse: response.data}))
+      .then(response => this.setState({omdbResponse: response.data.Search}))
     })    
   }
 
+  handleChoice(tt, title) {
+    this.setState(({tt: tt, title: title}),
+      console.log(this.state.tt)
+    )
+  }
 
   render() {
     return (
       <div className="App">
         <div className= "container">
-          <h3>Quotle</h3>
-          <div className="searchDiv">
-            <TextField label="Search" onInput={ this.handleChange } inputProps={ textFieldStyle } />
+          <div className="base">
+            <h1>Quotle</h1>
+            <div className="searchDiv">
+              <TextField label="Search" onInput={ this.handleChange } inputProps={ textFieldStyle } />
+            </div>
           </div>
           <div className="choiceDiv">
-            
+            {this.state.omdbResponse &&
+              this.state.omdbResponse.map(option => (
+                <div className="card" onClick={ () => this.handleChoice(option.imdbID, option.Title) }>
+                    <div className="card-image">
+                      <img src={ option.Poster } alt={ option.Title } />
+                    </div>
+                    <div className="card-text">
+                      <p>{ option.Title }</p>
+                    </div>
+                </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/materialTextField.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField'
-
-export default function textField() {
-    return (
-        <TextField id="standard-basic materialField" label="Search..."></TextField>
-    )
-}
