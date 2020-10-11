d1a169b529e4612d4d3f96b0514b64f166210794
LebowskiSama <anushvenkat101@gmail.com>
2020-10-11 18:22:38 +0530

    established a relay to the omdb service

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField'
+import axios from 'axios'
 
-const textFieldProps = {
-  color: "primary",
-  fullWidth: true
+const textFieldStyle = {
+  height: 300
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Quotle</h3>
-      <TextField id="standard-basic" label="Search" inputProps = {textFieldProps} />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      searchQuery: "",
+      omdbResponse: ""
+    }
+    
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({searchQuery: e.target.value}, () => {
+      axios.get("http://www.omdbapi.com/?apikey=2a5e533b&s="+this.state.searchQuery)
+      .then(response => this.setState({omdbResponse: response.data}))
+    })    
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className= "container">
+          <h3>Quotle</h3>
+          <div className="searchDiv">
+            <TextField label="Search" onInput={ this.handleChange } inputProps={ textFieldStyle } />
+          </div>
+          <div className="choiceDiv">
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
